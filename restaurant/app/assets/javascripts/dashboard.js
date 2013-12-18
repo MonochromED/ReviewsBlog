9d63bb0c27ca59be6f362cf594b31e900ce81acc
MonochromED <MonoChromEDUCD@gmail.com>
2013-12-18 00:43:50 -0800

    Improved Header Footer Format
    Header and Footer sticks to top and bottom regardless of vertical resize
    and initial starting window size.  Horizontal size does not auto adjust
    properly yet.

--- restaurant/app/assets/javascripts/dashboard.js
+++ restaurant/app/assets/javascripts/dashboard.js
@@ -3,14 +3,14 @@
 var news_content_height = "250px"
 var height_news_minimize_tab = "100%"
 $(document).ready(function(){
+
+
+	//News Tab effects.
 	$('#news_pull_tab').click(function(){
 		$(this).animate({borderTopLeftRadius:"0px"});
 		$('#news_content').animate({width:'200px', height:"+" + news_content_height },500).show();
         $('#news_minimize').animate({width:'260px', height:"+=" + height_news_minimize_tab}, 500 ).show();
 	    $("#news").animate({width:"260px"},500);
-
-
-
 	});
 
 	$('#news_minimize').click(function(){
@@ -19,6 +19,8 @@ $(document).ready(function(){
       $('#news_content, #news_minimize').animate({width:'100%', height:"100%"}, 500 ).hide(500);
 	});
 
+
+	//Decorative effects on navigation bar entities
 	$('.navigation_element').mouseenter(function(){
 		$(this).animate({color: "white" , backgroundColor: "#d2b48c"},800);
 	});
@@ -27,11 +29,32 @@ $(document).ready(function(){
 		$(this).animate({color: "#666666" , backgroundColor: "#112233"},400);
 	});
 
+
+	//ensure at initialization that header and footer are in correct positions
 	if ($(document).height() > $(window).height() ){
 		$("#footer").css({"position":"relative"});
 	}
 	else{
-		$("#footer").css({"position":"absolute"});
+		$("#footer").css({"position":"fixed"});
+		$("#header").css({"position":"absolute"});
 	}
+
+
+
+
+
 	
+});
+
+//ensure upon resize that header and footer are in correct positions
+$(window).resize(function(){
+	if ($(document).height() > $(window).height() ){
+		$("#footer").css({"position":"relative"});
+	}
+	else{
+		$("#footer").css({"position":"fixed"});
+		$("#header").css({"position":"absolute"});
+	}
+
+
 });
