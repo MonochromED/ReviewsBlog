aa7cc3c17ebd97434442860fb8ce5e4cafbe1d7f
MonochromED <MonoChromEDUCD@gmail.com>
2014-01-22 00:44:53 -0800

    Search bar focus
    Cursor focuses on search bar when the dropdown search area is triggered.

--- restaurant/app/assets/javascripts/searchdropdown.js
+++ restaurant/app/assets/javascripts/searchdropdown.js
@@ -1,6 +1,9 @@
+//Toggles search area drop down
 $(document).ready(function(){
 	$('#search_icon').click(function(){
-      $('#search_bar_area').slideToggle('slow'); 
+      $('#search_bar_area').slideToggle('slow');
+      //When search area drop down triggered, highlights search bar area to type into.
+      $('#search_text_bar').focus(); 
 	});
 
 
