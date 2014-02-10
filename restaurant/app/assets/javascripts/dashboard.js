f1dc00a2a4e04286d7884a83a858cb3b6f6d3c6a
MonochromED <MonoChromEDUCD@gmail.com>
2014-02-10 00:51:33 -0800

    News Tab and Header  Upgrade
    News Tab now uses only 1 button to toggle on and off.  Header now loads
    to proper size when JavaScript turned off.

--- restaurant/app/assets/javascripts/dashboard.js
+++ restaurant/app/assets/javascripts/dashboard.js
@@ -1,7 +1,6 @@
 //application javascript for dashboard view
 
 var news_content_height = "250px";
-var height_news_minimize_tab = "100%";
 
 //------Navigation Bar Effects----------------
 var hilite_nav_element_text_color;
@@ -30,6 +29,19 @@ function navigationElementEffectOff(){
 }
 
 //--------------------------------------------------
+//---------------News tab Expand Effect-------------
+function newsTabExpandEffectOn(){
+	$(this).animate({borderTopLeftRadius:"0px"});
+	$('#news_content').animate({width:'200px', height:"+" + news_content_height },500).show();
+    $("#news").animate({width:"260px"},500);
+}
+function newsTabExpandEffectOff(){
+    $('#news_pull_tab').animate({height:"250px", borderTopLeftRadius:"10px"},10);
+    $("#news").animate({width:"50px"},1000);
+    $('#news_content').animate({width:'100%', height:"100%"}, 500 ).hide(500);
+}
+//--------------------------------------------------
+
 
 //-----------------On Document Ready Actions-----------------------------------
 $(document).ready(function(){
@@ -39,16 +51,23 @@ $(document).ready(function(){
 
 	//News Tab effects.
 	$('#news_pull_tab').click(function(){
-		$(this).animate({borderTopLeftRadius:"0px"});
-		$('#news_content').animate({width:'200px', height:"+" + news_content_height },500).show();
-        $('#news_minimize').animate({width:'260px', height:"+=" + height_news_minimize_tab}, 500 ).show();
-        $("#news").animate({width:"260px"},500);
-	});
 
-	$('#news_minimize').click(function(){
-        $('#news_pull_tab').animate({height:"250px", borderTopLeftRadius:"10px"},10);
-        $("#news").animate({width:"50px"},1000);
-        $('#news_content, #news_minimize').animate({width:'100%', height:"100%"}, 500 ).hide(500);
+		if (!$(this).hasClass("news_tab_expanded")){
+			newsTabExpandEffectOn();
+			$(this).addClass("news_tab_expanded");
+		}
+		else{
+			newsTabExpandEffectOff();
+			$(this).removeClass("news_tab_expanded");
+		}
+
+
+	});
+	$('.news_tab_expanded').click(function(){
+		newsTabExpandEffectOn();
+		$(this).addClass("news_minimized");
+		$(this).removeClass("news_tab_expanded");
+		
 	});
 
 	//Colors navigation elements according to hover status and page location.
